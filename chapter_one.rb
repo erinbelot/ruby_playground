1ba0a8d8f2e8ad3a46051f5e40174231c627f00d
Erin Belot <erinbelot@gmail.com>
2018-09-16 21:28:32 +1000

    more ruby refresh

--- chapter_one.rb
+++ chapter_one.rb
@@ -1,27 +1,27 @@
-# flow_rate = 100
-# puts "The rate of flow is currently #{flow_rate}!"
-# flow_rate = 100 / 2
-# puts "The flow rate is now #{flow_rate}!"
+flow_rate = 100
+puts "The rate of flow is currently #{flow_rate}!"
+flow_rate = 100 / 2
+puts "The flow rate is now #{flow_rate}!"
 
-# flowmantic_on = true
-# water_available = true
-# if flowmantic_on && water_available 
-#     flow_rate = 50
-# elsif !flowmantic_on
-#     puts "flowmatic is off"
-# else
-#     puts "No water"
-# end
+flowmantic_on = true
+water_available = true
+if flowmantic_on && water_available 
+    flow_rate = 50
+elsif !flowmantic_on
+    puts "flowmatic is off"
+else
+    puts "No water"
+end
 
-# flow_rate = 40
-# unless flow_rate == 50
-#     puts "Warning flow rate is not at 50, it's #{flow_rate}"
-# end
+flow_rate = 40
+unless flow_rate == 50
+    puts "Warning flow rate is not at 50, it's #{flow_rate}"
+end
 
-# flow_rate = 50
-# if flow_rate < 50 || flow_rate > 50
-#     puts "Warning flow_rate is not at 50, it's #{flow_rate}"
-# end
+flow_rate = 50
+if flow_rate < 50 || flow_rate > 50
+    puts "Warning flow_rate is not at 50, it's #{flow_rate}"
+end
 
 flow_rate = 30
 if flow_rate != 50
